44922128bc7925fd757a3aec800e3ce36b93353f
Jagadeesh Thondrangi <jagadeesh_thondrangi@jabil.com>
2025-11-12 20:58:00 +0530

    updated

--- accounts/dev/vpc/main.tf
+++ accounts/dev/vpc/main.tf
@@ -1,11 +1,11 @@
 module "ipam" {
   source = "../../module/ipam_lab/"
   region = "ap-south-1"
-  supernet_cidr = "20.0.0.0/8"
-  regional_cidr = "20.0.0.0/12"
+  supernet_cidr = "24.0.0.0/8"
+  regional_cidr = "24.0.0.0/12"
   vpc_cidrs = {
-    dev  = "20.0.0.0/16"
-    test = "20.2.0.0/16"
+    dev  = "24.0.0.0/16"
+    test = "24.2.0.0/16"
   }
   vpc_disallowed_cidrs = {}
   vpc_account_ids = {}    
