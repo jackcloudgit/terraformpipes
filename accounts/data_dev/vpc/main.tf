cd0368e7779aa3687b7e80f8f297ac843fb70cac
Jagadeesh Thondrangi <jagadeesh_thondrangi@jabil.com>
2025-11-24 16:13:31 +0530

    updated

--- accounts/data_dev/vpc/main.tf
+++ accounts/data_dev/vpc/main.tf
@@ -1,15 +1,9 @@
-module "ipam" {
-  source = "../../module/ipam_lab/"
-  region = "ap-south-1"
-  supernet_cidr = "24.0.0.0/8"
-  regional_cidr = "24.0.0.0/12"
-  vpc_cidrs = {
-    dev  = "24.0.0.0/16"
-    test = "24.2.0.0/16"
-  }
-  vpc_disallowed_cidrs = {}
-  vpc_account_ids = {}    
-  
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket200244"
+  acl    = "private"
 
-#added comment
+  tags = {
+    Name        = "My example bucket"
+    Environment = "Dev"
+  }
+}
